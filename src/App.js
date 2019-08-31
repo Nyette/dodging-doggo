1d7aa7d2c75308abd0ae3fdaa0b96efbc7592250
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-08-31 17:41:37 -0400

    Set up wrapper component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Phaser from 'phaser';
+import { IonPhaser } from '@ion-phaser/react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    initialize: false,
+    game: {
+      width: "100%",
+      height: "100%",
+      type: Phaser.AUTO,
+      scene: {}
+    }
+  }
+  render() {
+    const { initialize, game } = this.state
+    return (
+      <IonPhaser game={game} initialize={initialize} />
+    )
+  }
 }
 
 export default App;
